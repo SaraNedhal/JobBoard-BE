2190ea038f0e7eb1bedafca9b483f9df65558f82
04maryam <itsmaryamali54@gmail.com>
2024-01-18 10:35:36 +0300

    updated

--- jobboard/main_app/views.py
+++ jobboard/main_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Skill, Profile, Company, Job_category, Job, Application
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
