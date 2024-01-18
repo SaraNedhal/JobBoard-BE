bdc71ca7e6eec2d848f76626547863f1cf50eacf
04maryam <itsmaryamali54@gmail.com>
2024-01-18 12:51:53 +0300

    urls updated

--- jobboard/jobboard/urls.py
+++ jobboard/jobboard/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(main_urls)),
-    path('api/', include('myapi.urls'))
 ]
