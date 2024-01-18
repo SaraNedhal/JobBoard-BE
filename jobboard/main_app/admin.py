42cb1b5e681ce3701ba50f3e8784ab82fd394c10
saranedhal <sarahnedhal@gmail.com>
2024-01-18 11:03:36 +0300

    fixed the profile model

--- jobboard/main_app/admin.py
+++ jobboard/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Skill, Profile, Company, Job_category, Job, Application
+
 
 # Register your models here.
+admin.site.register(Job)
+admin.site.register(Skill)
