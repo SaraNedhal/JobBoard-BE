f81547ad963b920b302e73f2dfb78c09d9016fa2
saranedhal <sarahnedhal@gmail.com>
2024-01-18 10:26:43 +0300

    fixed url in jobboard

--- jobboard/main_app/urls.py
+++ jobboard/main_app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   path('application/' , views.application_index , name="application_index"),
+  path('application/<int:user_id>/create/' , views.application_create, name="applications_create" ),
+ 
 ]
